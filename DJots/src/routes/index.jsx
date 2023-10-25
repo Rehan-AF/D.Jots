05b8587145494b3e2d6ac67d569e97d40d521a4f
Rehan-AF <af.rehan03@gmail.com>
2023-10-25 17:55:49 +0500

    FEAT: app setup routes, pages

--- DJots/src/routes/index.jsx
+++ DJots/src/routes/index.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from '../pages/home';
 
 const Routers = () => {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path="/" element={<div>Home</div>} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </Router>
     </div>
