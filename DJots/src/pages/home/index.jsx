4db7eed695531840719760d668238b1af333ed2c
Shan-barcha <shanbarcha11@gmil.com>
2023-10-26 21:10:53 +0500

    Feat:initial commit

--- DJots/src/pages/home/index.jsx
+++ DJots/src/pages/home/index.jsx
@@ -1,5 +1,16 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure rerum
+          vitae corrupti optio, unde ipsum, beatae veniam repudiandae,
+          consectetur id blanditiis cumque rem fuga architecto delectus sint
+          odio sunt culpa!
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default Home;
