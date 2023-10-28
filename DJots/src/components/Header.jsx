ca6484f72be5e289768e1236def55dc59538e7dd
Shan-barcha <shanbarcha11@gmil.com>
2023-10-29 00:01:17 +0500

    Feat:changes

--- DJots/src/components/Header.jsx
+++ DJots/src/components/Header.jsx
@@ -19,14 +19,14 @@ export default function Header() {
 
   return (
     <div className="bg-white">
-      <header className="absolute inset-x-0 top-0 z-50">
-        <div className="mx-auto max-w-7xl">
-          <div className="px-6 pt-6 lg:max-w-2xl lg:pl-6 lg:pr-0">
+      <div className="bg-[#2F2035]">
+        <div className="mx-auto max-w-7xl ">
+          <div className="px-6 p-4 lg:max-w-3xl lg:pl-6 lg:pr-0 w-full">
             <nav className="flex items-center justify-between lg:justify-start" aria-label="Global">
               <a href="#" className="-m-1.5 p-1.5">
                 <img
                   alt="Your Company"
-                  className="h-8 w-auto"
+                  className="h-12 w-12"
                   src={logo}
                 />
               </a>
@@ -38,61 +38,55 @@ export default function Header() {
                 <span className="sr-only">Open main menu</span>
                 <Bars3Icon className="h-6 w-6" aria-hidden="true" />
               </button>
-              <div className="hidden lg:ml-8 lg:flex lg:gap-x-8">
+              <div className="hidden lg:ml-8 lg:flex lg:gap-x-10">
                 {navigation.map((item) => (
-                  <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
+                  <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-white">
                     {item.name}
                   </a>
                 ))}
               </div>
             </nav>
           </div>
-        </div>
-        <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-          <div className="fixed inset-0 z-50" />
-          <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-            <div className="flex items-center justify-between">
-              <a href="#" className="-m-1.5 p-1.5">
-                <img
-                  className="h-8 w-auto"
-                  src={logo}
-                  alt=""
-                />
-              </a>
-              <button
-                type="button"
-                className="-m-2.5 rounded-md p-2.5 text-gray-700"
-                onClick={() => setMobileMenuOpen(false)}
-              >
-                <span className="sr-only">Close menu</span>
-                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-              </button>
-            </div>
-            <div className="mt-6 flow-root">
-              <div className="-my-6 divide-y divide-gray-500/10">
-                <div className="space-y-2 py-6">
-                  {navigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                    >
-                      {item.name}
-                    </a>
-                  ))}
-                </div>
-                <div className="py-6">
+        </div></div>
+      <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+        <div className="fixed inset-0 z-50" />
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <a href="#" className="-m-1.5 p-1.5 ">
+              <img
+                className="h-16 w-16 bg-black border rounded-lg "
+                src={logo}
+                alt=""
+              />
+            </a>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-gray-900"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                {navigation.map((item) => (
                   <a
-                    href="#"
-                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    key={item.name}
+                    href={item.href}
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
-                    Log in
+                    {item.name}
                   </a>
-                </div>
+                ))}
               </div>
             </div>
-          </Dialog.Panel>
-        </Dialog>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
+      <header className="absolute inset-x-0 top-0 z-50">
+
       </header>
 
       <div className="relative">
@@ -110,10 +104,10 @@ export default function Header() {
             <div className="relative px-6 py-24 md:py-22 sm:py-40 lg:px-8  lg:pr-0">
               <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl">
                 <h1 className="text-[30px] font-semibold tracking-tight lg:leading-[70px] text-[#3f065c] sm:text-6xl sans-serif">
-                Preserve and Secure your Efforts on the D.Jots
+                  Preserve and Secure your Efforts on the D.Jots
                 </h1>
                 <p className="mt-6 text-lg leading-8 text-gray-600">
-                Save Your Efforts, Save on Blockchain" safeguards your valuable work using blockchain technology. Our decentralized platform ensures the security and accessibility of your contributions, fostering a collaborative space for knowledge exchange. Join us in embracing the future of secure information sharing.
+                  Save Your Efforts, Save on Blockchain" safeguards your valuable work using blockchain technology. Our decentralized platform ensures the security and accessibility of your contributions, fostering a collaborative space for knowledge exchange. Join us in embracing the future of secure information sharing.
                 </p>
               </div>
             </div>
