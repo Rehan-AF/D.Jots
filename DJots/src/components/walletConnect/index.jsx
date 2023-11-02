6d361664f0e6fb4ff18ad0e1080e49a719e05502
Rehan-AF <af.rehan03@gmail.com>
2023-11-02 22:17:26 +0500

    FEAT: deploy new smart contract

--- DJots/src/components/walletConnect/index.jsx
+++ DJots/src/components/walletConnect/index.jsx
@@ -20,8 +20,9 @@ const Template = ({ children }) => {
   });
   // messgae 0x9e795874E53e745Badc7f44a682299B35d864307
   // app 0xE40439e97b32c3e1E8b4e45FCa9F86ae17d771a8
+  // app new 0x3d13c9613429753DF4A847B93184494aEcCB3998
   useEffect(() => {
-    const contractAddress = '0xE40439e97b32c3e1E8b4e45FCa9F86ae17d771a8';
+    const contractAddress = '0x3d13c9613429753DF4A847B93184494aEcCB3998';
     const contractABI = abi.abi;
 
     const loadBlockchainData = async () => {
