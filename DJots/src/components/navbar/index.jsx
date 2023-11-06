2d23a1d5739017a6a1d2f22ea83ca03d23e09e8d
Rehan-AF <af.rehan03@gmail.com>
2023-11-06 19:56:23 +0500

    FEAT: get notes data && post prediction && get predictions

---
+++ DJots/src/components/navbar/index.jsx
@@ -0,0 +1,99 @@
+import { Dialog } from '@headlessui/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.png';
+
+const navigation = [
+  { name: 'Home', href: '/' },
+  { name: 'Prediction', href: '/predictions' },
+  { name: 'Notes', href: '/notes' },
+  { name: 'Articles', href: '#' },
+  { name: 'Research Paper', href: '#' },
+  { name: 'Wallet Connect', href: '/Login' },
+];
+const Navbar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <div>
+      <div className="bg-[#2F2035]">
+        <div className="mx-auto max-w-7xl ">
+          <div className="px-6 p-4 lg:max-w-3xl lg:pl-6 lg:pr-0 w-full">
+            <nav
+              className="flex items-center justify-between lg:justify-start"
+              aria-label="Global"
+            >
+              <Link to="/" className="-m-1.5 ">
+                <img alt="Your Company" className=" w-12" src={logo} />
+              </Link>
+              <button
+                type="button"
+                className="-m-2.5 rounded-md p-2.5 text-gray-700 lg:hidden"
+                onClick={() => setMobileMenuOpen(true)}
+              >
+                <span className="sr-only">Open main menu</span>
+                <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+              </button>
+              <div className="hidden lg:ml-8 lg:flex lg:gap-x-10">
+                {navigation.map((item) => (
+                  <Link
+                    key={item.name}
+                    to={item.href}
+                    className="text-sm font-semibold leading-6 text-white"
+                  >
+                    {item.name}
+                  </Link>
+                ))}
+              </div>
+            </nav>
+          </div>
+        </div>
+      </div>
+      <Dialog
+        as="div"
+        className="lg:hidden"
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+      >
+        <div className="fixed inset-0 z-50" />
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <a href="#" className="-m-1.5 p-1.5 ">
+              <img
+                className="h-16 w-16 bg-black border rounded-lg "
+                src={logo}
+                alt=""
+              />
+            </a>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-gray-900"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                {navigation.map((item) => (
+                  <Link
+                    key={item.name}
+                    to={item.href}
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    {item.name}
+                  </Link>
+                ))}
+              </div>
+            </div>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
+    </div>
+  );
+};
+
+export default Navbar;
--- DJots/src/pages/login/index.jsx
+++ DJots/src/pages/login/index.jsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import loginimage from "../../assets/login.jpg"
-import metamask from "../../assets/metamask1.png"
+import loginimage from '../../assets/login.jpg';
+import metamask from '../../assets/metamask1.png';
 
 const Login = () => {
-    return (
-      <div className=' '>
-        <div className="flex flex-col  items-center justify-center  w-full bg-cover bg-center" 
-        style={{ backgroundImage: `url(${loginimage})`, height:"100vh", gap:"16px"}}
-        >
-          <div >
-            <img src={metamask} alt="logo" className='w-20'/>
-          </div>
-            <div className="flex items-center justify-center">
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-6 rounded">
-                    Wallet Connect
-                </button>
-            </div>
+  return (
+    <div>
+      <div
+        className="flex flex-col  items-center justify-center  w-full bg-cover bg-center"
+        style={{
+          backgroundImage: `url(${loginimage})`,
+          height: '100vh',
+          gap: '16px',
+        }}
+      >
+        <div>
+          <img src={metamask} alt="logo" className="w-20" />
         </div>
+        <div className="flex items-center justify-center">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-6 rounded">
+            Wallet Connect
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
